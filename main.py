1b0d16fa2b6bc9c51808e83cef92c3bd2f65da19
20195e <100391354+20195e@users.noreply.github.com>
2023-05-04 00:38:39 +0000

    test

--- main.py
+++ main.py
@@ -3,5 +3,8 @@ print ("Welcome to our Mini Movie Funraiser, we hope you enjoy the movie.")
 # Ask user if they need instructions.
 ans = input ("\nWould you like instructions?")
 
-if ans == "true" or "yes" or "sure" or "Indubitably":
-  print ("\n\nHere are some instructionsfor buying tickets.")
+if ans == "Yes" or "yes":
+  print ("\n\nHere are some instructions for buying tickets.")
+
+elif ans == "No" or "no":
+  print ("\n\naight cuh")
